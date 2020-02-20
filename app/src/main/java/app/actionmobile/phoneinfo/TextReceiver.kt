155f645fc31124969c80aa28fdbd61b2d3c49952
raddevus <1radeutsch1@gmail.com>
2020-02-20 11:47:42 -0500

    Working on receiving MMS messages.  Now I know when a mms or sms message is received using only one class TextReceiver. However, cannot retrieve data from mms yet.

--- app/src/main/java/app/actionmobile/phoneinfo/TextReceiver.kt
+++ app/src/main/java/app/actionmobile/phoneinfo/TextReceiver.kt
@@ -3,26 +3,46 @@ package app.actionmobile.phoneinfo
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.os.Bundle
-import android.provider.Telephony
 import android.provider.Telephony.Sms.Intents.getMessagesFromIntent
 import android.telephony.SmsMessage
 import android.util.Log
+import android.widget.Toast
 import app.actionmobile.phoneinfo.ui.main.PlaceholderFragment
 
 class TextReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context?, intent: Intent?) {
+        Toast.makeText(context!!,"Got a text", Toast.LENGTH_LONG).show()
         Log.d("MainActivity", "got a text")
-        var bundle = intent?.extras
-        var allMessages : Array<SmsMessage> = getMessagesFromIntent(intent)
-        var msg : String
-        var from : String? = null
-        for (m in allMessages){
-            msg = m.messageBody
-            from = m.originatingAddress
-            Log.d("MainActivity", "from: $from -- $msg")
-            PlaceholderFragment.handleTextMessageArrival("from: $from? -- $msg")
+        try {
+            var bundle = intent?.extras
+            try {
+                var allMessages: Array<SmsMessage> = getMessagesFromIntent(intent)
+
+                var msg: String
+                var from: String? = null
+                for (m in allMessages) {
+                    msg = m.messageBody
+                    from = m.originatingAddress
+                    Log.d("MainActivity", "from: $from -- $msg")
+                    PlaceholderFragment.handleTextMessageArrival("from: $from? -- $msg")
+                }
+            }
+            catch (ex: Exception){
+                PlaceholderFragment.handleTextMessageArrival("Location 1: ${ex.message!!}")
+                Log.d("MainActivity", ex.message)
+            }
+            val mmsBundle = intent!!.extras
+            var mmsData  = mmsBundle!!.getByteArray("data")
+            Log.d("MainActivity","${mmsData}")
+            PlaceholderFragment.handleTextMessageArrival("mmsData : ${mmsData.toString()}")
+
+
+
+        }
+        catch (ex : Exception){
+            PlaceholderFragment.handleTextMessageArrival("Location 2: ${ex.message!!}")
+            Log.d("MainActivity", ex.message)
         }
     }
 
